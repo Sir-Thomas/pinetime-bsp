b255fa4012ed819620f3b8fadef7cb3c2952ac19
Sir-Thomas <tp8153@gmail.com>
2026-02-13 21:03:24 -0600

    simplify and give control to OS

--- src/backlight.rs
+++ src/backlight.rs
@@ -6,8 +6,7 @@ pub struct BacklightController {
     low: Output<'static>,
     medium: Output<'static>,
     high: Output<'static>,
-    brightness_level: InternalBrightnessLevel,
-    enabled: bool,
+    brightness_level: BrightnessLevel,
 }
 
 impl BacklightController {
@@ -21,44 +20,31 @@ impl BacklightController {
             low: Output::new(backlight_low_pin, Level::High, OutputDrive::Standard),
             medium: Output::new(backlight_med_pin, Level::High, OutputDrive::Standard),
             high: Output::new(backlight_high_pin, Level::High, OutputDrive::Standard),
-            brightness_level: InternalBrightnessLevel::Medium,
-            enabled: false,
+            brightness_level: BrightnessLevel::default(),
         }
     }
 
     /// Get the current brightness level
     pub fn brightness(&self) -> BrightnessLevel {
-        if !self.enabled {
-            BrightnessLevel::Off
-        } else {
-            match self.brightness_level {
-                InternalBrightnessLevel::Low => BrightnessLevel::Low,
-                InternalBrightnessLevel::Medium => BrightnessLevel::Medium,
-                InternalBrightnessLevel::High => BrightnessLevel::High,
-            }
-        }
+        self.brightness_level
     }
 
-    fn internal_set_brightness(&mut self, level: InternalBrightnessLevel) {
+    /// Set the brightness level
+    pub fn set_brightness(&mut self, level: BrightnessLevel) {
         self.low.set_high();
         self.medium.set_high();
         self.high.set_high();
         self.brightness_level = level;
         match level {
-            InternalBrightnessLevel::Low => self.low.set_low(),
-            InternalBrightnessLevel::Medium => self.medium.set_low(),
-            InternalBrightnessLevel::High => self.high.set_low(),
+            BrightnessLevel::Low => self.low.set_low(),
+            BrightnessLevel::Medium => self.medium.set_low(),
+            BrightnessLevel::High => self.high.set_low(),
         }
     }
 
-    /// Set the brightness level
-    pub fn set_brightness(&mut self, level: BrightnessLevel) {
-        match level {
-            BrightnessLevel::Off => self.disable(),
-            BrightnessLevel::Low => self.internal_set_brightness(InternalBrightnessLevel::Low),
-            BrightnessLevel::Medium => self.internal_set_brightness(InternalBrightnessLevel::Medium),
-            BrightnessLevel::High => self.internal_set_brightness(InternalBrightnessLevel::High),
-        }
+    /// Enable the backlight at the current brightness level
+    pub fn enable(&mut self) {
+        self.set_brightness(self.brightness_level);
     }
 
     /// Disable the backlight
@@ -66,40 +52,17 @@ impl BacklightController {
         self.low.set_high();
         self.medium.set_high();
         self.high.set_high();
-        self.enabled = false;
-    }
-
-    /// Enable the backlight at the last set brightness level
-    pub fn enable(&mut self) {
-        self.internal_set_brightness(self.brightness_level);
-        self.enabled = true;
-    }
-
-    /// Toggle the backlight
-    pub fn toggle(&mut self) {
-        match self.enabled {
-            true => self.disable(),
-            false => self.enable(),
-        }
     }
 }
 
 /// Brightness levels for the backlight
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Default)]
 pub enum BrightnessLevel {
-    /// Backlight off
-    Off,
     /// Low brightness
+    #[default]
     Low,
     /// Medium brightness
     Medium,
     /// High brightness
-    High,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-enum InternalBrightnessLevel {
-    Low,
-    Medium,
     High,
 }
